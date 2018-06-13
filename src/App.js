4aeec2b330f2957173dc7796c2d3be7848a7adcd
ntdang <ndang83@gmail.com>
2018-06-12 22:17:45 -0400

    Cards show and shuffle. Score not correct yet

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import FriendCard from "./components/FriendCard";
 import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Container from "./Container";
+import Row from "./Row";
+import Column from "./Column";
 import friends from "./friends.json";
 import "./App.css";
 
-function shuffleFriends(array) {
+function friendsArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
@@ -23,12 +27,22 @@ class App extends Component {
     clicked: [],
   };
 
+  // when img is clicked
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
   // handleIncrement increments this.state.count by 1
   handleIncrement = () => {
     const newScore = this.state.currentScore + 1;
     this.setState({
       currentScore: newScore,
-      message: "Good pick!"
+      message: "Good catch!"
     });
     if (newScore >= this.state.topScore) {
       this.setState({ topScore: newScore });
@@ -39,27 +53,54 @@ class App extends Component {
     this.handleShuffle();
   };
 
+  // reset when img is clicked twice
   handleReset = () => {
     this.setState({
       currentScore: 0,
       topScore: this.state.topScore,
-      message: "!",
+      message: "Oh no! You already caught this one!",
       clicked: []
     });
     this.handleShuffle();
   };
 
+  handleShuffle = () => {
+    let shuffledFriends = friendsArray(friends);
+    this.setState({ friends: shuffledFriends });
+  };
 
   render() {
     return (
       <Wrapper>
-      <FriendCard
-        image={friends[0].image}
-      />
-      <FriendCard
-      image={friends[1].image}
-    />
-    </Wrapper>
+        <Nav
+          title="Pokemon Clicky Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          message={this.state.message}
+        />
+
+        <Title>
+          Try to catch 'em all! But don't catch the same one twice or you'll lose!
+        </Title>
+
+        <Container>
+          <Row>
+            {this.state.friends.map(friend => (
+              <Column size="md-3 sm-6">
+                <FriendCard
+                  key={friend.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={friend.id}
+                  image={friend.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
     );
   }
 }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,2 +1,20 @@
 import React from "react";
 import "./Nav.css";
+
+const Nav = props => (
+  <nav>
+    <ul>
+      <li className="nav-item active">
+        <a href="/react-clicky-game/">{props.title}</a>
+      </li>
+
+      <li id="msg">{props.message}</li>
+
+      <li id="current">Current Score: {props.score}</li>
+
+      <li id="top">Top Score: {props.topScore}</li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
